3780c117f99c14e965b7dd772291247b38b952cc
Asad <asadmhmd1234@gmail.com>
2022-02-01 20:08:47 +0530

    Refactor route code to controllers and add star rating system
    Use starability.css to implement star rating system and refactor routes using router.route() method.

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,37 +1,17 @@
 const express = require("express");
-const Campground = require("../models/campground");
-const Review = require("../models/review");
 const catchAsync = require("../utils/catchAsync");
+const reviews = require("../controllers/reviews");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 
 const router = express.Router({ mergeParams: true });
 
-router.post(
-  "/",
-  validateReview,
-  catchAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    review.author = req.user._id;
-    await review.save();
-    await campground.save();
-    req.flash("success", "Successfully added review!");
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
+router.post("/", validateReview, catchAsync(reviews.createReview));
 
 router.delete(
   "/:reviewId",
   isLoggedIn,
   isReviewAuthor,
-  catchAsync(async (req, res, next) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review deleted successfully!");
-    res.redirect(`/campgrounds/${id}`);
-  })
+  catchAsync(reviews.deleteReview)
 );
 
 module.exports = router;
